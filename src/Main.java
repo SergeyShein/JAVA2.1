a688c423dc2750a704a0a62c299d9f6b3521df98
Sergey Shein <sergeyshein20@gmail.com>
2020-12-28 23:57:03 +0300

    Fist commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_balance = 2_000_000_000;
+        int refill = 500_000_000;
+        int total = current_balance + refill;
+        System.out.println(total);
+    }
+}
